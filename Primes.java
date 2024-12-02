35c0fc53b37c71916ab7ee106371acca73f62216
Larabruk <larabrukirer@gmail.com>
2024-12-02 22:32:50 +0200

    Updated

--- Primes.java
+++ Primes.java
@@ -4,22 +4,16 @@ public class Primes {
         System.out.println("Prime numbers up to " + n + ":");
         boolean[] arr = new boolean[n + 1];
         int counter = 0;
-        for(int i = 2; i < n + 1; i++){
+        for(int i = 2; i <= n; i++){
             arr[i] = true;
         }
         for (int p = 2; p <= Math.sqrt(n); p++) {
-            for (int i = 0; i < arr.length; i++)
-            {
-                if (i == p || i % p == 0) {
-                    arr[i] = false;
-                }
+            if(arr[p]){
+                for(int i = p * p; i <= n; i+=p){
+                arr[i] = false;
             }
         }
-        for(int i = 0; i < n; i++){
-            if(i == 2 || i == 3 || i == 5){
-                arr[i] = true;
-            }
-        }
+    }
         for (int i = 0; i < arr.length; i++){
             if (arr[i] == true) {
                 counter++;
